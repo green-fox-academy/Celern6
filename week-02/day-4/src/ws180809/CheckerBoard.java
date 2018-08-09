cec6fed6c326c9fa21553e3bfc8d38558c5ee134
Andras Toth <celern6@gmail.com>
2018-08-09 17:29:42 +0200

    Workshop save 18/08/09

--- week-02/day-4/src/ws180809/CheckerBoard.java
+++ week-02/day-4/src/ws180809/CheckerBoard.java
@@ -7,12 +7,24 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class CheckerBoard {
   public static void mainDraw(Graphics graphics) {
     int i = 0;
+    int k = 0;
+
     while (i <= 320) {
       graphics.setColor(Color.BLACK);
       graphics.fillRect(0 + i, 0, 40, 40);
-      i =+ 80;
+      graphics.fillRect(0 + i, 80, 40, 40);
+      graphics.fillRect(0 + i, 160, 40, 40);
+      graphics.fillRect(0 + i, 240, 40, 40);
+      i += 80;;
     }
-  }
+    while (k < 280) {
+      graphics.setColor(Color.BLACK);
+      graphics.fillRect(40 + k, 40, 40, 40);
+      graphics.fillRect(40 + k, 120, 40, 40);
+      graphics.fillRect(40 + k, 200, 40, 40);
+      graphics.fillRect(40 + k, 280, 40, 40);
+      k += 80;
+    }}
 
   // Don't touch the code below
   static int WIDTH = 320;
