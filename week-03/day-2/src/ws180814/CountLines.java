a67bc21aa38abd18ec40ef79c98fafe2774d0447
András Tóth <celern6@gmail.com>
2018-08-14 13:33:22 +0200

    CountLines.java

---
+++ week-03/day-2/src/ws180814/CountLines.java
@@ -0,0 +1,28 @@
+package ws180814;
+
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CountLines{
+  public static void main(String[] args) {
+    try {
+     Path filepath = Paths.get("valami.txt");
+     List<String> lines = Files.readAllLines(filepath);
+     int counter = 1;
+     for (String i : lines)
+     {
+       counter += 1;}
+       return counter;
+     } catch (Exception e){
+     System.out.println("0");
+     }
+
+  }
+}
