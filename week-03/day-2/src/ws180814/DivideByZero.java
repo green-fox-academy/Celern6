4fb84d55ceeb2c56cf59913bf5e807df16f2e4eb
András Tóth <celern6@gmail.com>
2018-08-14 11:56:17 +0200

    Added DivideByZero

---
+++ week-03/day-2/src/ws180814/DivideByZero.java
@@ -0,0 +1,22 @@
+package ws180814;
+
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print "fail" if the parameter is 0
+
+import java.util.Scanner;
+
+public class DivideByZero {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please enter a number");
+    int number = scanner.nextInt();
+    try {
+      int result = 10/number;
+      System.out.println(result);
+    } catch (ArithmeticException e) {
+      System.out.println("fail");
+    }
+  }
+}
