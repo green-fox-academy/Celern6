0d1d5df64b782dbf55581fdf45a8c7ecb61d22d4
András Tóth <celern6@gmail.com>
2018-08-15 16:51:37 +0200

    Added partly working Dominoes

---
+++ week-03/day-3/DiceSet/src/diceset/Main.java
@@ -0,0 +1,10 @@
+package diceset;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        DiceSet diceSet = new DiceSet();
+      System.out.println(diceSet.getCurrent());
+    }
+}
---
+++ week-03/day-3/Dominoes/src/dominoes/Dominoes.java
@@ -0,0 +1,42 @@
+package dominoes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dominoes {
+    public static void main(String[] args) {
+        List<Domino> dominoes = initializeDominoes();
+        // You have the list of Dominoes
+        // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
+        // eg: [2, 4], [4, 3], [3, 5] ...
+      System.out.println(dominoes.get(0).getValues()[1]);
+      System.out.println(dominoes);
+      System.out.println(arrangedominoes(dominoes));
+
+    }
+
+    static List<Domino> initializeDominoes() {
+        List<Domino> dominoes = new ArrayList<>();
+        dominoes.add(new Domino(5, 2));
+        dominoes.add(new Domino(4, 6));
+        dominoes.add(new Domino(1, 5));
+        dominoes.add(new Domino(6, 7));
+        dominoes.add(new Domino(2, 4));
+        dominoes.add(new Domino(7, 1));
+        return dominoes;
+    }
+    static List<Domino> arrangedominoes(List<Domino> indominoes){
+      List<Domino> fixeddominoes = new ArrayList<>();
+      fixeddominoes.add(indominoes.get(0));
+      Integer j = 0;
+      while (fixeddominoes.size() < indominoes.size()){
+        for(int i = 0; i < indominoes.size(); i++){
+          if (fixeddominoes.get(j).getValues()[1] == indominoes.get(i).getValues()[0]) {
+          fixeddominoes.add(indominoes.get(i));
+          j = +1;
+          }
+        }
+      }
+      return fixeddominoes;
+    }
+}
