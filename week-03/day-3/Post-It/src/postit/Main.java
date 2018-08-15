f5ae7d263409afa2ddeb5ff0a2723df00a297ae2
András Tóth <celern6@gmail.com>
2018-08-15 12:06:03 +0200

    Added PostIt

---
+++ week-03/day-3/Post-It/src/postit/Main.java
@@ -0,0 +1,27 @@
+package postit;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    PostIt postit1 = new PostIt();
+    postit1.backgroundColor = "orange";
+    postit1.text = "Idea1";
+    postit1.textColor = "Blue";
+
+    PostIt postit2 = new PostIt();
+    postit2.backgroundColor = "pink";
+    postit2.text = "Awesome";
+    postit2.textColor = "Black";
+
+    PostIt postit3 = new PostIt();
+    postit3.backgroundColor = "yellow";
+    postit3.text = "Superb!";
+    postit3.textColor = "Green";
+
+
+    }
+
+
+    }
+
---
+++ week-03/day-3/Post-It/src/postit/PostIt.java
@@ -0,0 +1,7 @@
+package postit;
+
+public class PostIt {
+  public String backgroundColor;
+  public String text;
+  public String textColor;
+}
