ba11ad74496776f31c820959982e2af1b57f9588
András Tóth <celern6@gmail.com>
2018-08-16 16:01:53 +0200

    added BunniesAgain.java

--- week-03/day-4/src/ws180816/BunniesAgain.java
+++ week-03/day-4/src/ws180816/BunniesAgain.java
@@ -7,5 +7,19 @@ package ws180816;
 
 
 public class BunniesAgain {
-
+  public static void main(String[] args) {
+    int bunnies = 5;
+    System.out.println(furanyuszik(bunnies));
+  }
+  public static Integer furanyuszik (int nyuszik){
+    int ketfulu = 2;
+    int haromfulu = 3;
+    if (nyuszik == 1){
+      return ketfulu;
+    } else if (nyuszik % 2 == 0){
+      return haromfulu + furanyuszik(nyuszik-1);
+    } else {
+      return ketfulu + furanyuszik(nyuszik-1);
+    }
+  }
 }
