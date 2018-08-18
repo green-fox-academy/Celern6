e16a295fde4541c496ac3b0e0eefc0361bad590c
András Tóth <celern6@gmail.com>
2018-08-18 11:58:07 +0200

    reorganizing day one

--- week-01/day-4/src/Workshop/HelloMe.java
+++ week-01/day-4/src/Workshop/HelloMe.java
@@ -35,53 +35,6 @@ class HelloUser {
 
 
 
-
-// Write a program that asks for 5 integers in a row,
-// then it should print the sum and the average of these numbers like:
-//
-// Sum: 22, Average: 4.4
-
-
-// Write a program that reads a number from the standard input,
-// Then prints "Odd" if the number is odd, or "Even" if it is even.
-
-// Write a program that reads a number form the standard input,
-// If the number is zero or smaller it should print: Not enough
-// If the number is one it should print: One
-// If the number is two it should print: Two
-// If the number is more than two it should print: A lot
-
-
-
-
-// Write a program that asks for two numbers
-// The first number represents the number of girls that comes to a party, the
-// second the boys
-// It should print: The party is excellent!
-// If the the number of girls and boys are equal and 20 or more people are coming to the party
-//
-// It should print: Quite cool party!
-// If there are 20 or more people coming to the party but the girl - boy ratio is not 1-1
-//
-// It should print: Average party...
-// If there are less people coming than 20
-//
-// It should print: Sausage party
-// If no girls are coming, regardless the count of the people
-
-
-
-
-class IWontcheatontheexams {
-  public static void main(String[] args) {
-    int a = 0;
-    while (a < 100) {
-      System.out.println("I wont cheat in the exam.");
-      a += 1;
-    }
-  }
-}
-
 class Printeven {
   public static void main(String[] args) {
     int a = 0;
---
+++ week-01/day-4/src/Workshop/IWontcheatontheexams.java
@@ -0,0 +1,11 @@
+package Workshop;
+
+public class IWontcheatontheexams {
+  public static void main(String[] args) {
+    int a = 0;
+    while (a < 100) {
+      System.out.println("I wont cheat in the exam.");
+      a += 1;
+    }
+  }
+}
