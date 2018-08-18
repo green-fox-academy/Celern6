794f5e5defc660c3f52faa86eb7522225218371e
András Tóth <celern6@gmail.com>
2018-08-18 11:15:11 +0200

    reorganizing day one

---
+++ week-01/day-4/src/Workshop/TwoNumbers.java
@@ -0,0 +1,31 @@
+package Workshop;
+
+class TwoNumbers {
+  public static void main(String[] args) {
+    // Create a program that prints a few operations on two numbers: 22 and 13
+
+    // Print the result of 13 added to 22
+
+    // Print the result of 13 substracted from 22
+
+    // Print the result of 22 multiplied by 13
+
+    // Print the result of 22 divided by 13 (as a decimal fraction)
+
+    // Print the integer part of 22 divided by 13
+
+    // Print the reminder of 22 divided by 13
+    int a;
+    int b;
+    a = 13;
+    b = 22;
+
+    System.out.println(a+b);
+    System.out.println(b-a);
+    System.out.println(b*a);
+    System.out.println(22./13.);
+    System.out.println(b/a);
+    System.out.println((22./13.)-1);
+
+  }
+}
