4b132aff3a14f70af68e3f0020990ec2d69f028e
András Tóth <celern6@gmail.com>
2018-08-18 11:13:48 +0200

    reorganizing day one

--- week-01/day-4/src/Workshop/HelloMe.java
+++ week-01/day-4/src/Workshop/HelloMe.java
@@ -9,23 +9,7 @@ class HelloMe {
 
 
 
-class IntroduceYourself {
-  public static void main(String[] args) {
-    // Write a program that prints a few details to the terminal window about you
-    // It should print each detail to a new line.
-    //  - Your name
-    //  - Your age
-    //  - Your height in meters (as a decimal fraction)
-    //
-    //  Example output:
-    //  John Doe
-    //  31
-    //  1.87
-    System.out.println("András Tóth");
-    System.out.println("33");
-    System.out.println("1.85");
-  }
-}
+
 class TwoNumbers {
   public static void main(String[] args) {
     // Create a program that prints a few operations on two numbers: 22 and 13
