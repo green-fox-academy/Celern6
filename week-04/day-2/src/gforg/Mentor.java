0ceefec787f537cbe3d5d5de76a7d415426912d7
András Tóth <celern6@gmail.com>
2018-08-21 13:24:32 +0200

    Added Mentor.java class

---
+++ week-04/day-2/src/gforg/Mentor.java
@@ -0,0 +1,21 @@
+package gforg;
+
+public class Mentor extends Person{
+  String level;
+
+  public void getGoal() {
+    System.out.println("Educate brilliant junior software developers.");
+  }
+
+  public void introduce() {
+    System.out.println("Hi, i'm " + name + " a " + age + " year old " + gender + " " + level + " mentor.");
+  }
+  public Mentor (String name, int age, String gender, String level){
+  }
+  Mentor (){
+    name = "Jane Doe";
+    age = 30;
+    gender = "female";
+    level = "Intermediate";
+  }
+}
