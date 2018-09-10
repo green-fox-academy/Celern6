acbe0c940472a40b898fb12dc4fa61766c613221
András Tóth <celern6@gmail.com>
2018-09-10 12:53:06 +0200

    Added exercise1

---
+++ week-06/day-1/src/day1/Exercise1.java
@@ -0,0 +1,14 @@
+package day1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+	// write your code here
+      ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+
+      numbers.stream().filter(x -> x%2 == 0).forEach(System.out :: println);
+    }
+}
