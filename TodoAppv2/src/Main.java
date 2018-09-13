f41157f926fd0e76d7cafed1a7d50d95a5b97e5e
András Tóth <celern6@gmail.com>
2018-09-13 16:54:40 +0200

    Added remove basic function to ToDo App v2 with error handling

--- TodoAppv2/src/Main.java
+++ TodoAppv2/src/Main.java
@@ -16,15 +16,17 @@ public class Main {
 
         if (args[0].equals("-a")){
           if(args.length < 2.){
-            System.out.println("Unable to add: no task provided");
+            System.out.println("Unable to add: no task provided.");
             } else {
             mytoDo.addToList(args[1]);
           }
         }
 
-        if (args[0].equals("-r")){
-          if(args.length < 2.){
-            System.out.println("Unable to remove: no index provided");
+        if (args[0].equals("-r")) {
+          if (args.length < 2.) {
+            System.out.println("Unable to remove: no index provided.");
+          } else if (Integer.parseInt(args[1]) > args.length){
+            System.out.println("Unable to remove: index is out of bond.");
           } else {
             mytoDo.removeFromList(args[1]);
           }
