ee85f2dff91202a4562371c49dd8125dc9c30567
András Tóth <celern6@gmail.com>
2018-09-18 13:24:12 +0200

    Created Bankaccount class with fields and constructors

---
+++ week-07/day-1/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/Model/BankAccount.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.bankofsimba.Model;
+
+public class BankAccount {
+  private String name;
+  private long balance;
+  private String animalType;
+
+  public BankAccount(String name, long balance, String animalType){
+    this.name = name;
+    this.balance = balance;
+    this.animalType = animalType;
+  }
+
+}
