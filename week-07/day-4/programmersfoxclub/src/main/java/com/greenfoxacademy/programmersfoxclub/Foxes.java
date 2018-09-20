9edef41d84c4d529b119578765f084a19f819baf
András Tóth <celern6@gmail.com>
2018-09-20 15:48:24 +0200

    Updated Fox Class

---
+++ week-07/day-4/programmersfoxclub/src/main/java/com/greenfoxacademy/programmersfoxclub/Foxes.java
@@ -0,0 +1,11 @@
+package com.greenfoxacademy.programmersfoxclub;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class Foxes {
+  List<Fox> Foxlist = new ArrayList<>();
+}
--- week-07/day-4/programmersfoxclub/src/main/java/com/greenfoxacademy/programmersfoxclub/controllers/MainController.java
+++ week-07/day-4/programmersfoxclub/src/main/java/com/greenfoxacademy/programmersfoxclub/controllers/MainController.java
@@ -1,6 +1,8 @@
 package com.greenfoxacademy.programmersfoxclub.controllers;
 
 
+import com.greenfoxacademy.programmersfoxclub.Foxes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +12,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainController {
 
+  @Autowired
+  Foxes foxes;
+
+  @GetMapping("/")
+  public String main (@RequestParam(value = "name", required = false) String name, Model model){
+    if (name != null){
+    model.addAttribute("name", name);
+
+    return "index";
+    } else {
+      return "redirect:/login";
+    }
+  }
+
 
   @GetMapping("/login")
   public String login (Model model){
     return "login";
   }
 
-  @PostMapping()
+  @PostMapping("/login")
   public String namepost (@RequestParam("name") String name){
-    return "index";
+    return "redirect:/?name="+name;
   }
 }
