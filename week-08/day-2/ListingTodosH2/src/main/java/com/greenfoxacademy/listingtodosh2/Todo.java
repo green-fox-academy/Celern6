910666f17b8c3615372752bc0d696ff4dff89746
András Tóth <celern6@gmail.com>
2018-09-25 13:53:11 +0200

    Updated Controller methods and class, added repository

--- week-08/day-2/ListingTodosH2/src/main/java/com/greenfoxacademy/listingtodosh2/Todo.java
+++ week-08/day-2/ListingTodosH2/src/main/java/com/greenfoxacademy/listingtodosh2/Todo.java
@@ -6,6 +6,35 @@ public class Todo {
   boolean urgent = false;
   boolean done = false;
 
+  public int getId() {
+    return id;
+  }
 
+  public void setId(int id) {
+    this.id = id;
+  }
 
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public boolean isUrgent() {
+    return urgent;
+  }
+
+  public void setUrgent(boolean urgent) {
+    this.urgent = urgent;
+  }
+
+  public boolean isDone() {
+    return done;
+  }
+
+  public void setDone(boolean done) {
+    this.done = done;
+  }
 }
