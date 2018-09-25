14a18ab47bc1dba026b4f50f108166f99b81f24b
András Tóth <celern6@gmail.com>
2018-09-25 13:47:08 +0200

    Updated Controller methods and class

--- week-08/day-2/ListingTodosH2/src/main/java/com/greenfoxacademy/listingtodosh2/TodoController.java
+++ week-08/day-2/ListingTodosH2/src/main/java/com/greenfoxacademy/listingtodosh2/TodoController.java
@@ -4,6 +4,7 @@ package com.greenfoxacademy.listingtodosh2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TodoController {
@@ -15,6 +16,7 @@ public class TodoController {
   }
 
   @GetMapping(value = {"/", "/list"})
+  @ResponseBody
   public String list(Model model){
     return "This is my first todo";
   }
